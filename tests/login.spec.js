19caac8383cd0aba610d16115067acfb8c04f706
Amritasree-JL <jlamritasreenair@gmail.com>
2026-09-07 00:45:47 +0530

    Add logout test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -18,3 +18,15 @@ test('Invalid password shpws error message',async({page}) =>{
 
     await expect(page.getByText('Invalid credentials')).toBeVisible();
 });
+
+test('User can logout',async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByPlaceholder('Username').fill('Admin');
+    await page.getByPlaceholder('Password').fill('admin123');
+    await page.getByRole('button',{name:'Login'}).click();
+    await expect(page).toHaveURL(/dashboard/);
+    await page.getByAltText('profile picture').click();
+    await page.getByRole('menuitem',{name:'Logout'}).click();
+
+    await expect(page).toHaveURL(/auth\/login/);
+});

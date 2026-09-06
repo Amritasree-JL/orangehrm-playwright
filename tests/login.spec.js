ed30836dfd81b0c311f379f33a49e5b8516736f0
Amritasree-JL <jlamritasreenair@gmail.com>
2026-09-06 23:53:37 +0530

    Add negative login test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -9,3 +9,12 @@ test('User login with valid credentials',async({page})=>{
     await expect(page).toHaveURL(/dashboard/);
 
 });
+
+test('Invalid password shpws error message',async({page}) =>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByPlaceholder('Username').fill('Admin');
+    await page.getByPlaceholder('Password').fill('wrongpassword');
+    await page.getByRole('button',{name : 'Login'}).click();
+
+    await expect(page.getByText('Invalid credentials')).toBeVisible();
+});
